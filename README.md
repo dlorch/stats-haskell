7c5c8bfab6a9f7fa1c05141d219f9274959ab396
Daniel Lorch <dlorch@gmail.com>
2017-02-09 23:03:50 +0100

    Implemented frequency, mode, mean, variance, standard deviation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Statistics in Haskell Playground
+
+Teaching myself statistics and Haskell at the same time.
+
+Loosely based on the excellent book [Think Stats 2e](http://greenteapress.com/wp/think-stats-2e/) by Allen B. Downey.
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,36 @@
 module Main where
 
+import NSFG
+import Data.List (group, sort, genericLength)
+
+frequencies xs = map countGroups (group (sort xs))
+    where countGroups x = (head x, length x)
+
+mode xs = findMax (frequencies xs)
+    where findMax ((val, freq):[]) = val
+          findMax ((val1, freq1):(val2, freq2):xs) = if freq1 > freq2
+                                                     then findMax ((val1, freq1):xs)
+                                                     else findMax ((val2, freq2):xs)
+
+mean xs = realToFrac (sum xs) / genericLength xs
+
+var xs = sum (map squared (map (deviationFromMean xs) xs)) / genericLength xs
+    where squared x = x * x
+          deviationFromMean xs = \x -> (realToFrac x) - (mean xs)
+
+std xs = sqrt (var xs)
+
 main :: IO ()
-main = putStrLn "someFunc"
+main = do
+    -- http://www.icpsr.umich.edu/nsfg6/Controller?displayPage=labelDetails&fileCode=PREG&section=&subSec=8016&srtLabel=611932
+    print (frequencies fempreg_outcome)
+    -- http://www.icpsr.umich.edu/nsfg6/Controller?displayPage=labelDetails&fileCode=PREG&section=&subSec=8014&srtLabel=611802
+    print (frequencies fempreg_birthwgt_lb)
+    -- most common birth weight is 7 pounds
+    print (mode fempreg_birthwgt_lb)
+    -- for all live births, mean pregnancy length is 38.6
+    print (mean fempreg_live_prglngth)
+    -- for all live births, variance of pregnagncy length is 7.3
+    print (var fempreg_live_prglngth)
+    -- for all live births, standard deviation is 2.7 weeks
+    print (std fempreg_live_prglngth)
