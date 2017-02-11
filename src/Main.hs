3ce322a2d954c74e8b5cfd82ff4e667cdf308eba
Daniel Lorch <dlorch@gmail.com>
2017-02-11 09:51:53 +0100

    Compute mean and variance of PMF

--- src/Main.hs
+++ src/Main.hs
@@ -27,6 +27,14 @@ sampleSize ((val, freq):xs) = freq + (sampleSize xs)
 normalize xs = map (divideBySampleSize (sampleSize xs)) xs
     where divideBySampleSize sampleSize = \(val, freq) -> (val, (realToFrac freq) / (realToFrac sampleSize))
 
+pmfMean xs = sum (map probTimesVal xs)
+    where probTimesVal (val, prob) = (realToFrac val) * prob
+
+pmfVar xs = sum (map sumBody xs)
+    where meanValue = pmfMean xs
+          squared x = x * x
+          sumBody (val, prob) = prob * (squared (val - meanValue))
+
 main :: IO ()
 main = do
     -- http://www.icpsr.umich.edu/nsfg6/Controller?displayPage=labelDetails&fileCode=PREG&section=&subSec=8016&srtLabel=611932
@@ -41,6 +49,10 @@ main = do
     print (var fempreg_live_prglngth)
     -- for all live births, standard deviation is 2.7 weeks
     print (std fempreg_live_prglngth)
-    -- PMF is {1: 0.2, 2: 0.4, 3: 0.2, 5: 0.2}
+
     let freqs = frequencies [1, 2, 2, 3, 5]
-    print (normalize freqs)
+    -- PMF is {1: 0.2, 2: 0.4, 3: 0.2, 5: 0.2}
+    let pmf = normalize freqs
+    print pmf
+    print (pmfMean pmf)
+    print (pmfVar pmf)
